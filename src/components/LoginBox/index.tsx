f88515d17f4ef751bff8af47cfa6f71f26e6fb50
douglasconstancio <douglas.pconstancio@gmail.com>
2021-10-23 13:12:41 -0300

    feat: add message list integrated with backend

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,12 +1,50 @@
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
+
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
+
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=2ba7f9645d85d0c62f6c`
+
+  async function signIn(code: string) {
+    const response = await api.post<AuthResponse>('authenticate', { code })
+
+    const { token, user } = response.data
+
+    localStorage.setItem('@dowhile:token', token)
+
+    console.log(user)
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=')
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode)
+    }
+  }, [])
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size={24}/>
         Entrar com Github
       </a>
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from 'react';
+
+import logoImg from '../../assets/logo.svg';
+import { api } from '../../services/api';
+
+import styles from './styles.module.scss';
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data)
+    })
+  }, [])
+
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile2021" />
+
+      <ul className={styles.messageList}>
+      { messages.map(message => (
+        <li key={message.id} className={styles.message}>
+          <p className={styles.messageContent}>{message.text}</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src={message.user.avatar_url} alt={message.user.name} />
+            </div>
+            <span>{message.user.name}</span>
+          </div>
+        </li>
+      )) }
+      </ul>
+    </div>
   )
 }
