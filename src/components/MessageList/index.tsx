6d7c0ebe8559883b631b9e439480c5951b932f13
douglasconstancio <douglas.pconstancio@gmail.com>
2021-10-25 08:59:46 -0300

    refact: add message box management and update socket connection

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
 import logoImg from '../../assets/logo.svg';
 import { api } from '../../services/api';
@@ -14,8 +15,32 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ].filter(Boolean))
+
+        messagesQueue.shift()
+      }
+    }, 3000)
+
+    return () => clearInterval(interval);
+  }, [])
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then(response => {
